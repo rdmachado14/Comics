a9f431435cf656207ba2883459bae0da30956912
Carlos Amadheus Souza Araujo <K_Amadheus@lovegod-mac.local>
2015-09-08 16:29:23 -0400

    Dando zoom

--- ComicsSolution/ComicsSolution/ViewController.swift
+++ ComicsSolution/ComicsSolution/ViewController.swift
@@ -11,26 +11,30 @@ import UIKit
 class ViewController: UIViewController
 {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var comicImage: UIImageView!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        self.scrollView.minimumZoomScale = 1.0;
+        self.scrollView.maximumZoomScale = 6.0;
+        
         loadImage()
-        
-        
     }
 
-    override func didReceiveMemoryWarning()
-    {
+    override func didReceiveMemoryWarning(){
         super.didReceiveMemoryWarning()
     }
     
-    func loadImage()
-    {
+   func loadImage() {
         comicImage.image = UIImage(named: "s005") // carregar imagem na tela
     }
 
-
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView?
+    {
+        return self.comicImage
+    }
 }
 
