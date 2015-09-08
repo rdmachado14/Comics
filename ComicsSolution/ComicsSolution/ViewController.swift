02b9aeaeb671d656821555e191956fcd4b747cd6
leonardo fernandes farias <leonardo@macbook-pro-de-leonardo.local>
2015-09-08 14:43:33 -0400

    adicionando os sons e o reconhecimentos dos swipes

--- ComicsSolution/ComicsSolution/ViewController.swift
+++ ComicsSolution/ComicsSolution/ViewController.swift
@@ -7,16 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController
 {
 
     @IBOutlet weak var comicImage: UIImageView!
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         loadImage()
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view!.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view!.addGestureRecognizer(swipeLeft)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view!.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view!.addGestureRecognizer(swipeDown)
+        
+        let swipeHorario=UIRotationGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        self.view!.addGestureRecognizer(swipeHorario)
         
         
     }
@@ -29,6 +50,64 @@ class ViewController: UIViewController
     func loadImage()
     {
         comicImage.image = UIImage(named: "s005") // carregar imagem na tela
+    }
+    
+    
+    
+    func respondToSwipeGesture(gesture: UIGestureRecognizer){
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer{
+            switch swipeGesture.direction{
+            case UISwipeGestureRecognizerDirection.Right:
+                print("right")
+                audioPlayer = setupAudioPlayerWithFile("Pagina 1", type: "mp3")
+                audioPlayer.play()
+                comicImage.image = UIImage(named: "s006")
+                return
+                
+            case UISwipeGestureRecognizerDirection.Left:
+                
+                print("left")
+                audioPlayer = setupAudioPlayerWithFile("Pagina 1", type: "mp3")
+                audioPlayer.play()
+                comicImage.image = UIImage(named: "s005")
+
+                
+                return
+                
+            case UISwipeGestureRecognizerDirection.Up:
+                print("up")
+                
+                return
+                
+            case UISwipeGestureRecognizerDirection.Down:
+                print("down")
+                
+                //                    audioPlayer.prepareToPlay()
+                //                    audioPlayer.volume = 5.0
+                //                    audioPlayer.numberOfLoops = 20
+                
+                return
+                
+            default:
+                return
+            }
+        }
+        
+        print("rodou")
+    }
+    
+    func setupAudioPlayerWithFile(file:NSString, type:NSString) -> AVAudioPlayer  {
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
+        let url = NSURL.fileURLWithPath(path!)
+        var audioPlayer:AVAudioPlayer?
+        
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        } catch {
+            print("NO AUDIO PLAYER")
+        }
+        
+        return audioPlayer!
     }
 
 
