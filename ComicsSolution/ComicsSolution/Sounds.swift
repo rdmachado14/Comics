fd532ad2b80fa0cebc510c55a7a36bb7ba73ce42
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-09-09 11:45:51 -0400

    Criando as pastas.

---
+++ ComicsSolution/ComicsSolution/Sounds.swift
@@ -0,0 +1,9 @@
+//
+//  Sounds.swift
+//  ComicsSolution
+//
+//  Created by Rodrigo Machado on 9/9/15.
+//  Copyright © 2015 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import Foundation
