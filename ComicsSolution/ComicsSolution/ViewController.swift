aea5f32043abd9574142af90b2de8791ce10a0dc
leonardo fernandes farias <leonardo@macbook-pro-de-leonardo.local>
2015-09-10 11:10:28 -0400

    colocando as fotas no vetor

--- ComicsSolution/ComicsSolution/ViewController.swift
+++ ComicsSolution/ComicsSolution/ViewController.swift
@@ -16,14 +16,15 @@ class ViewController: UIViewController
     @IBOutlet weak var comicImage: UIImageView!
     var audioPlayer = AVAudioPlayer()
     var sound = Sound()
-    
+    var cont = Int()
+    var vetorStrings = ["p1","p2","p3","p4"]
     var doubleTap = true
     var location = CGPoint()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        loadImage()
+        loadImage(vetorStrings)
         loadSwipe()
         loadTapGesture()
         
@@ -31,15 +32,14 @@ class ViewController: UIViewController
         self.scrollView.minimumZoomScale = 1.0;
         self.scrollView.maximumZoomScale = 6.0;
         
-        loadImage()
     }
 
     override func didReceiveMemoryWarning(){
         super.didReceiveMemoryWarning()
     }
     
-   func loadImage() {
-        comicImage.image = UIImage(named: "s005") // carregar imagem na tela
+    func loadImage(vetor: [String]) {
+        comicImage.image = UIImage(named: vetor[cont]) // carregar imagem na tela
     }
     
     func loadSwipe() {
@@ -91,17 +91,26 @@ class ViewController: UIViewController
             switch swipeGesture.direction{
             case UISwipeGestureRecognizerDirection.Right:
                 print("right")
-                audioPlayer = sound.setupAudioPlayerWithFile("Pagina 1")
-                audioPlayer.play()
-                comicImage.image = UIImage(named: "s006")
+                if cont < vetorStrings.count - 1 {
+                    cont++
+                    audioPlayer = sound.setupAudioPlayerWithFile("Pagina 1")
+                    audioPlayer.play()
+                }
+                
+                
+                comicImage.image = UIImage(named: vetorStrings[cont])
                 return
                 
             case UISwipeGestureRecognizerDirection.Left:
                 
                 print("left")
-                audioPlayer = sound.setupAudioPlayerWithFile("Pagina 1")
-                audioPlayer.play()
-                comicImage.image = UIImage(named: "s005")
+                if cont > 0 {
+                    cont--
+                    audioPlayer = sound.setupAudioPlayerWithFile("Pagina 1")
+                    audioPlayer.play()
+                }
+                
+                comicImage.image = UIImage(named: vetorStrings[cont])
 
                 
                 return
