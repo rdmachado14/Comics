d95f5dc84452186229f3abfdf400247d79bbdaaa
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-09-17 11:15:07 -0400

    Adicionando alguns comentários.

--- ComicsSolution/ComicsSolution/ViewController.swift
+++ ComicsSolution/ComicsSolution/ViewController.swift
@@ -232,22 +232,24 @@ class ViewController: UIViewController
     
     @IBAction func animation(sender: AnyObject)
     {
-        for i in 0...5
+        for _ in 0...5
         {
+            // iniciando a animação
             view.addSubview(comicImage)
             
-            let randomYOffset = CGFloat(arc4random_uniform(150))
+            let randomYOffset = CGFloat(arc4random_uniform(150)) // cria um valor aleatório entre 0 e 150
             let path = UIBezierPath()
             path.moveToPoint(CGPoint(x: 16, y: 239 + randomYOffset))
             path.addCurveToPoint(CGPoint(x: 301, y: 239 + randomYOffset), controlPoint1: CGPoint(x: 136, y: 373 + randomYOffset), controlPoint2: CGPoint(x: 178, y: 110 + randomYOffset))
             
+            // criando a animação
             let anim = CAKeyframeAnimation(keyPath: "position")
             anim.path = path.CGPath
             anim.rotationMode = kCAAnimationRotateAuto
             anim.repeatCount = Float.infinity
             anim.duration = 5.0
             
-            // add the animation
+            // adicionando a animação
             comicImage.layer.addAnimation(anim, forKey: "animate position along path")
         
         }
